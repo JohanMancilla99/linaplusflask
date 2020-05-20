165fc819f0c23f420787915cb7f7f1aa59651ef4
Johan <johanmancilla99@gmail.com>
2020-05-19 21:28:58 -0500

    se agrego la interfaz de admin anterior, solo para pruebas

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ app = Flask(__name__)
 def landing():
     return render_template("landing.html")
 
+# ruta de administrador
+@app.route('/admin')
+def adminRoute():
+    return render_template('admin/menuAdministrador.html')
+
 
 if (__name__ == "__main__"):
     app.run(debug = True)
